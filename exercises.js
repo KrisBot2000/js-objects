652666c741d43fed056cbaf07953f5f7898f907a
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-05-17 17:06:35 -1000

    added Addition with an object

--- exercises.js
+++ exercises.js
@@ -53,7 +53,7 @@ kicks.buy = function(){
 
 console.log(kicks);
 console.log(kicks.brand);
-console.log(kicks.buy);
+console.log(kicks.buy());
 
 
 
@@ -71,6 +71,8 @@ var plainBox = {};
 plainBox.color = "green";
 plainBox.size = 13;
 plainBox.contents = [];
+
+console.log(plainBox);
 
 /*
    Declare a variable named `stockCar` and create the following properties (key-value pairs) to the object:
@@ -151,11 +153,77 @@ console.log(completePerson.age);
             ...
  */
 
-//var arrayOfObjects = (https://gist.github.com/sgnl/958adf99007329d2e4ff);
+var arrayOfObjects =
+[
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
 
 function printProcessedOrders(orders){
+  for(var i = 0; i<orders.length; i++){
+    console.log("=====");
+    console.log("id: " + orders[i].id);
+    console.log("purchase date: " + orders[i].date);
+    console.log("purchase total: " + orders[i].total);
+
+  }
 
 }
+
+
+
+
+
+
+
 
 /*
    Addition with an object
@@ -204,6 +272,12 @@ console.log(sumObjResult);
         Invoke this function and pass in your object. Further test by changing the values of the object being passed in or 
         **create more** objects and invoke your function multiple times.
  */
+
+//function printObj(object){
+
+//console.log(object.a + " + " object.b + " = " )
+
+//}
 
 
 /*
